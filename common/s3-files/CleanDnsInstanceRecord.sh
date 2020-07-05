e9ae50ba99f21c4c11c190616e4b83df173bac17
Karles <karles@karlesnine.com>
2020-07-05 18:52:10 +0200

    [*DnsInstanceRecord] improve script

--- common/s3-files/CleanDnsInstanceRecord.sh
+++ common/s3-files/CleanDnsInstanceRecord.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-# (optional) You might need to set your PATH variable at the top here
-# depending on how you run this script
-# PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+set -ex
 
 #
-# FOR RUN ON AMZ LINUX 1 - Elasticbeanstalk
-# OR FOR Ubuntu >= 16;04
+# RUN ON AMZ LINUX 1 - Elasticbeanstalk
+# RUN ON Ubuntu >= 16;04
 #
 
-set -e
+# Hosted Zone ID 
+ZONEID="Z0749049F7V9D4X8X30S"
+# The domain or sub domain you want to update e.g. hello.example.com
+DOMAINE="aws.karlesnine.com"
 
+# DNS record advanced options below
+# The Time-To-Live of this recordset
+TTL=7200
+# Change this if you want
+COMMENT="Auto updating @ `date`"
+# Change to AAAA if using an IPv6 address
+TYPE="A"
+
+
+#
+# Information gathering from AWS about the intance
+# 
 IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | awk '{print substr($1, 0, length($1)-1)}')
@@ -28,9 +41,8 @@ ENV_NAME=$(aws ec2 describe-tags \
     --query "Tags[*].Value")
 
 # BUILD THE HOSTNAME
-# Hostname come from tag name for ubuntu instance
-# Hostname come from elasticbeanstalk env name and IP for elasticbeanstalk instance amz linux
-#
+#   Hostname come from tag name for ubuntu instance
+#   Hostname come from elasticbeanstalk env name and IP for elasticbeanstalk instance amz linux
 if [ ! -z $AUTO_SCALING_GROUP_NAME ];
 then
     if [ -z $ENV_NAME]
@@ -44,20 +56,11 @@ else
 fi
 
 
-# Hosted Zone ID e.g. BJBK35SKMM9OE
-# In VC it's vcaws.com
-ZONEID="Z0749049F7V9D4X8X30S"
-# The CNAME you want to update e.g. hello.example.com
-RECORDSET="$NEWHOSTNAME.aws.karlesnine.com"
-# More advanced options below
-# The Time-To-Live of this recordset
-TTL=7200
-# Change this if you want
-COMMENT="Auto updating @ `date`"
-# Change to AAAA if using an IPv6 address
-TYPE="A"
-
+#
+# Build Json file for Route 53
+#
 function json_file {
+    RECORDSET="$NEWHOSTNAME.$DOMAINE"
     TMPFILE=$(mktemp /tmp/temporary-file.XXXXXXXX)
     cat > ${TMPFILE} << EOF
     {
@@ -81,11 +84,16 @@ function json_file {
 EOF
 }
 
+#
+# Apply Json file to Route 53
+#
 function set_dns_record {
     # Update the Hosted Zone record
     aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://"$TMPFILE"
 }
 
-
+#
+# Built it, run it
+#
 json_file;
 set_dns_record;
